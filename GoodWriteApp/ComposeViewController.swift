0219fb900d9e6f3267cb0ba1f38588c55bc0c09d
junhyuk <dejavukk1@gmail.com>
2020-04-28 15:35:48 +0900

    새로운 메모 화면 추가.

---
+++ GoodWriteApp/ComposeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ComposeViewController.swift
+//  GoodWriteApp
+//
+//  Created by junhyuk on 2020/04/28.
+//  Copyright © 2020 junhyuk. All rights reserved.
+//
+
+import UIKit
+
+class ComposeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
